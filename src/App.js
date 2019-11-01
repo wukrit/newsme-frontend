f063c696de488bd65a231a96b46181f62146668c
wukrit <sukritwalia@gmail.com>
2019-11-01 18:05:55 -0400

    created initial containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-  return <div class='main-container'></div>
+  return <div class='main-container'>
+
+  </div>
 }
 
 export default App
--- src/Redux/Reducers/userReducer.js
+++ src/Redux/Reducers/userReducer.js
@@ -3,8 +3,7 @@ const initialState = {}
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'SET_USER':
-      return { ...state, ...payload }
-
+      return { ...payload }
     case 'CLEAR_USER':
         return {}
     default:
