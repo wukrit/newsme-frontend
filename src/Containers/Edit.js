fca46d8a88d43089991319853c3cf1c773273be4
wukrit <sukritwalia@gmail.com>
2019-11-04 21:15:38 -0500

    conditionally rendered form

--- src/Containers/Edit.js
+++ src/Containers/Edit.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 function Edit({ state, dispatch }) {
+  const renderForm = () => (
+    <form className='control'>
+      <label className='label'>Name:</label>
+      <input className='input' type='text' defaultValue={state.user.name} />
+    </form>
+  )
+
   return (
     <>
       <nav className='navbar'></nav>
       <div className='container is-widescreen'>
         <h2 className='title'>Hey, {state.user ? state.user.name : null}</h2>
         <p className='subtitle'>Edit Your Settings</p>
+        <div className='column'>{state.user ? renderForm() : null}</div>
       </div>
     </>
   )
