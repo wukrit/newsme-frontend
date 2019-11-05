0f8a5c77c99cb3780b95090f2cfadd1cec279579
wukrit <sukritwalia@gmail.com>
2019-11-04 20:42:45 -0500

    Added newsfeed greeting, started edit container

--- src/Containers/Edit.js
+++ src/Containers/Edit.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-function Edit() {
-    return (
-        <div>
-            This is the Edit container
-        </div>
-    )
+function Edit({ state, dispatch }) {
+  return (
+    <>
+      <nav className='navbar'></nav>
+      <div className='container is-widescreen'>
+        <h2 className='title'>Hey, {state.user ? state.user.name : null}</h2>
+        <p className='subtitle'>Edit Your Settings</p>
+      </div>
+    </>
+  )
 }
 
-export default Edit
+const mapStateToProps = state => ({ state: state.user })
+const mapDispatchToProps = dispatch => ({
+  dispatch
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Edit)
