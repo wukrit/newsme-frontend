7352e2164c12fa4137ef12a758e53b79dd3f4f47
Wukrit <sukritwalia@gmail.com>
2019-11-07 10:53:10 -0500

    Created topic form component

---
+++ src/Components/TopicForm.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+
+function TopicForm({ topicState, breaks, newUser }) {
+  if (newUser) {
+    return topicState.topics.map(topic => {
+      return (
+        <Fragment key={`${topic.id}-label`}>
+          <label className='checkbox'>
+            <input type='checkbox' name={topic.title} />
+            {topic.title}
+          </label>
+          {breaks === true ? <br /> : null}
+        </Fragment>
+      )
+    })
+  } else {
+    const subbed_topics = topicState.subs.map(sub => sub.topic_id)
+    return topicState.topics.map(topic => {
+      return subbed_topics.includes(topic.id) ? (
+        <Fragment key={`${topic.id}-label`}>
+          <label className='checkbox'>
+            <input type='checkbox' name={topic.title} defaultChecked />
+            {topic.title}
+          </label>
+          {breaks === true ? <br /> : null}
+        </Fragment>
+      ) : (
+        <Fragment key={`${topic.id}-label`}>
+          <label className='checkbox'>
+            <input type='checkbox' name={topic.title} />
+            {topic.title}
+          </label>
+          {breaks === true ? <br /> : null}
+        </Fragment>
+      )
+    })
+  }
+}
+
+const mapStateToProps = state => ({
+  topicState: state.topics
+})
+export default connect(
+  mapStateToProps,
+  null
+)(TopicForm)
