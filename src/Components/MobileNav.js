ae12db63ea2408cda44195543e60404a86269676
Wukrit <sukritwalia@gmail.com>
2019-11-12 14:49:23 -0500

    added mobile sign in

--- src/Components/MobileNav.js
+++ src/Components/MobileNav.js
@@ -1,11 +1,33 @@
 import React, { useRef } from 'react'
+import UserActions from '../Redux/Actions/userActions'
+import SignupActions from '../Redux/Actions/signupActions'
+import { connect } from 'react-redux'
 import '../Styles/MobileNav.css'
 
-function MobileNav() {
+function MobileNav({ state, login, setLoginEmail, setLoginPassword }) {
   const menuRef = useRef(null)
 
   const handleToggleMenu = () => {
     menuRef.current.classList.toggle('open')
+  }
+
+  const handleEmailChange = event => {
+    setLoginEmail(event.target.value)
+  }
+
+  const handlePasswordChange = event => {
+    setLoginPassword(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const userObj = {
+      email: state.loginEmail,
+      password: state.loginPassword
+    }
+    login(userObj)
+    event.target.reset()
+    return false
   }
 
   return (
@@ -15,9 +37,43 @@ function MobileNav() {
       </div>
       <div ref={menuRef} className='sidenav'>
         <li onClick={handleToggleMenu}><i className="fas fa-times closebtn"></i></li>
+        <form
+              className='field is-horizontal mobile'
+              onSubmit={event => handleSubmit(event)}
+            >
+              <input
+                className='input is-small'
+                name='email'
+                type='email'
+                placeholder='Email'
+                onChange={event => handleEmailChange(event)}
+                value={state.loginEmail}
+              />
+              <input
+                className='input is-small'
+                name='password'
+                type='password'
+                placeholder='Password'
+                onChange={event => handlePasswordChange(event)}
+                value={state.loginPassword}
+              />
+              <input
+                className='button is-small is-link'
+                type='submit'
+                value='Login'
+              />
+            </form>
       </div>
     </>
   )
 }
 
-export default MobileNav
+const mapStateToProps = state => ({ state: state.signup, user: state.user })
+
+const mapDispatchToProps = {
+  login: UserActions.login,
+  setLoginEmail: SignupActions.setLoginEmail,
+  setLoginPassword: SignupActions.setLoginPassword
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MobileNav)
